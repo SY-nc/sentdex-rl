1a0bdb99d767131cc773d9f41a34a342b32964f4
SY-nc <satish000yadav@gmail.com>
2022-06-25 21:15:10 +0530

    hard-coded coordinates

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import numpy as np
 #import keras.backend.tensorflow_backend as backend
 import keras.backend as backend
-##from keras import models ### Added for PASS (01/06/2022)
-from keras import models
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Activation, Flatten
 # from keras.optimizers import Adam
@@ -22,39 +20,31 @@ DISCOUNT = 0.99
 REPLAY_MEMORY_SIZE = 50_000  # How many last steps to keep for model training
 # can use _ in place of , # we take a random sampling of those 50,000 steps and thats the batch to train the NN (provide stability & smoothing)
 MIN_REPLAY_MEMORY_SIZE = 1_000  # Minimum number of steps in a memory to start training
-# MINIBATCH_SIZE = 64  # How many steps (samples) to use for training
-MINIBATCH_SIZE = 1
-# UPDATE_TARGET_EVERY = 5  # Terminal states (end of episodes)
-UPDATE_TARGET_EVERY = 1
+MINIBATCH_SIZE = 64  # How many steps (samples) to use for training
+UPDATE_TARGET_EVERY = 5  # Terminal states (end of episodes)
 MODEL_NAME = '2x256' # whatever you want, as here NN 
 MIN_REWARD = -200  # For model save
 MEMORY_FRACTION = 0.20 # not using right now
 
 # Environment settings
-# EPISODES = 20_000
-
-#for testing model
-EPISODES = 1
+EPISODES = 20_000
 
 # Exploration settings
 epsilon = 1  # not a constant, going to be decayed
-#epsilon = 0.6 ### Added for PASS (01/06/2022)
-#epsilon = 0.0
 EPSILON_DECAY = 0.99975
 MIN_EPSILON = 0.001
 
 # Stats settings
-AGGREGATE_STATS_EVERY = 50  #  episodes (step aggregation)
-# AGGREGATE_STATS_EVERY = 1  # episodes
+AGGREGATE_STATS_EVERY = 500  #  episodes (step aggregation)
 # SHOW_PREVIEW = False # set to true if we want to actually see the visuals of everything running 
 SHOW_PREVIEW = False
 
 # ADD BLOB CLASS (Copy & Paste) 
 class Blob:
-    def __init__(self, size):
+    def __init__(self, size, xx, yy):
         self.size = size
-        self.x = np.random.randint(0, size)
-        self.y = np.random.randint(0, size)
+        self.x = xx
+        self.y = yy
 
     def __str__(self):
         return f"Blob ({self.x}, {self.y})"
@@ -126,8 +116,8 @@ class Blob:
             illegal.append(0)
             illegal.append(2)
             illegal.append(6)
-        
         if len(illegal) > 0:
+             
             legal = []
             for i in range(1, 7+1):
                 if(i not in illegal):
@@ -135,11 +125,11 @@ class Blob:
             rand_index = np.random.randint(0, len(legal))
             new_action = legal[rand_index]
             self.action(new_action)
+
 ### ADD Blob Environment (copy & paste)
 
 class BlobEnv:
     SIZE = 10
-    # SIZE = 20
     RETURN_IMAGES = True
     MOVE_PENALTY = 1
     ENEMY_PENALTY = 300
@@ -155,13 +145,9 @@ class BlobEnv:
          3: (0, 0, 255)}
 
     def reset(self):
-        self.player = Blob(self.SIZE)
-        self.food = Blob(self.SIZE)
-        while self.food == self.player:
-            self.food = Blob(self.SIZE)
-        self.enemy = Blob(self.SIZE)
-        while self.enemy == self.player or self.enemy == self.food:
-            self.enemy = Blob(self.SIZE)
+        self.player = Blob(self.SIZE, 0, 0)
+        self.food = Blob(self.SIZE, 9, 9)
+        self.enemy = Blob(self.SIZE, 0, 9)
 
         self.episode_step = 0
 
@@ -194,9 +180,7 @@ class BlobEnv:
             reward = -self.MOVE_PENALTY
 
         done = False
-        # if reward == self.FOOD_REWARD or reward == -self.ENEMY_PENALTY or self.episode_step >= 200:
-        if reward == self.FOOD_REWARD or self.episode_step >= 200:
-        #if reward == self.FOOD_REWARD or reward == -self.ENEMY_PENALTY or self.episode_step >= 1250:
+        if reward == self.FOOD_REWARD or self.episode_step >= 200: ## For Constraint
             done = True
 
         return new_observation, reward, done
@@ -253,33 +237,32 @@ if not os.path.isdir('models'):
 #        self.step = 1
 #        self.writer = tf.summary.create_file_writer(self.log_dir)
 #        self._log_write_dir = self.log_dir
-   
 #    # Overriding this method to stop creating default log writer
 #    def set_model(self, model):
 #        self.model = model
-   
+#    
 #        self._train_dir = os.path.join(self._log_write_dir, 'train')
 #        self._train_step = self.model._train_counter
-
+#
 #        self._val_dir = os.path.join(self._log_write_dir, 'validation')
 #        self._val_step = self.model._test_counter
-
+#
 #        self._should_write_train_graph = False
-   
+#    
 #    # Overrided, saves logs with our step number
 #    # (otherwise every .fit() will start writing from 0th step)
 #    def on_epoch_end(self, epoch, logs=None):
 #        self.update_stats(**logs)
-   
+#    
 #    # Overrided
 #    # We train for one batch only, no need to save anything at epoch end
 #    def on_batch_end(self, batch, logs=None):
 #        pass
-   
+#    
 #    # Overrided, so won't close writer
 #    def on_train_end(self, _):
 #        pass
-   
+#    
 #    # Custom method for saving own metrics
 #    # Creates writer, writes custom metrics and closes writer
 #    def update_stats(self, **stats):
@@ -328,6 +311,7 @@ class ModifiedTensorBoard(TensorBoard):
      def update_stats(self, **stats):
          self._write_logs(stats, self.step)
 
+
 # Agent class
 class DQNAgent:
     def __init__(self): # init method for this model 
@@ -336,8 +320,27 @@ class DQNAgent:
     ## Two models: for stability and consistency to learn something because initially so much randomness
 
         # Main model # gets trained every step
-        self.model = models.load_model("models/6.1.3-400-ep-done.model")
+        self.model = self.create_model() ### Commented for PASS (01/06/2022)
+        #self.model = models.load_model("./models/2x256____25.00max_-142.34avg_-461.00min__1654127755.model")
+        #self.model = models.load_model("./models/2x256____24.00max_-170.96avg_-455.00min__1654173105.model")
+        
+        #self.model = models.load_model("./models/2x256____25.00max__-94.36avg_-457.00min__1654346256.model") ## Model for new RUN (18:07)
+        #self.model = models.load_model("./models/2x256____25.00max___-4.20avg_-200.00min__1654354834.model") ## Model for new RUN (20:30)-2nd RUN
+        
+        
+        #self.model = models.load_model("./models/2x256____24.00max_-186.70avg_-490.00min__1654818493.model") ## Model for 1st PASS (10/06/2022,05:18)
+        #self.model = models.load_model("./models/2x256____25.00max_-115.88avg_-444.00min__1654857443.model") ## Model for 2nd PASS (10/06/2022,16:07)
+        
+        #self.model = models.load_model("./models/2x256____25.00max_-269.77avg_-12758.00min__1655001850.model") ## Model for 1st PASS (12/06/2022,08:14)
+        #self.model = models.load_model("./models/2x256____25.00max_-195.81avg_-11263.00min__1655024713.model") ## Model for 2nd PASS (12/06/2022,14:35)
+        #self.model = models.load_model("./models/2x256____25.00max_-272.54avg_-10964.00min__1655056069.model") ## Model for 3rd PASS (12/06/2022,23:17)
+        #self.model = models.load_model("./models/2x256____25.00max_-245.34avg_-16047.00min__1655079956.model") ## Model for 4th PASS (13/06/2022,05:55)
+        
+        
+        #self.model = models.load_model("./models/2x256____50.00max__-71.88avg_-5496.00min__1655243335.model") ## Model for 1st PASS (15/06/2022,03:18)
+        
         #self.model.compile(loss="mse", optimizer=Adam(lr=0.001), metrics=['accuracy']) ### Added for PASS (01/06/2022)
+
         # Target model this is what we .predict against every step
         self.target_model = self.create_model() # This is the model that we are doing .predict every step 
         self.target_model.set_weights(self.model.get_weights()) # same weights (After every some no. of episodes you will re-update your target model)
@@ -369,8 +372,6 @@ class DQNAgent:
 
         model.add(Dense(env.ACTION_SPACE_SIZE, activation='linear'))  # ACTION_SPACE_SIZE = how many choices (9)
         model.compile(loss="mse", optimizer=Adam(lr=0.001), metrics=['accuracy']) # we will track accuracy
-        # model.compile(loss="mse", optimizer=Adam(lr=0.001), metrics=['mse']) ## Changes 'accuracy' to 'mse'
-        #model.compile(loss= "mean_squared_error" , optimizer="adam", metrics=["mean_squared_error"]) ## From Internet
         return model
 
     # Adds step's data to a memory replay array
@@ -387,12 +388,12 @@ class DQNAgent:
     ## Add to 5.1 
     # add new train method
     # Trains main network every step during episode
-    def test(self, terminal_state, step):
+    def train(self, terminal_state, step):
 
 
         # Start training only if certain number of samples is already saved
-        # if len(self.replay_memory) < MIN_REPLAY_MEMORY_SIZE:
-        #     return
+        if len(self.replay_memory) < MIN_REPLAY_MEMORY_SIZE:
+            return
 
         # Get a minibatch of random samples from memory replay table
         minibatch = random.sample(self.replay_memory, MINIBATCH_SIZE)
@@ -428,12 +429,8 @@ class DQNAgent:
             X.append(current_state) # inputs
             y.append(current_qs) # outputs
 
-        # actual_val = self.get_qs(np.array(X))
-        # print("X: " + str(X))
-        # print("y: " + str(y))
-        # print("actual: " + str(actual_val))
         # Fit on all samples as one batch, log only on terminal state
-        # self.model.fit(np.array(X)/255, np.array(y), batch_size=MINIBATCH_SIZE, verbose=0, shuffle=False, callbacks=[self.tensorboard] if terminal_state else None)
+        self.model.fit(np.array(X)/255, np.array(y), batch_size=MINIBATCH_SIZE, verbose=0, shuffle=False, callbacks=[self.tensorboard] if terminal_state else None)
         # shuffle=False because we have already grabbed a random sampling, custom callback, we will fit all of this if on our terminal state O/W we will fit nothing
 
         # Update target network counter every episode (updating to determine if we want to update target_model yet)
@@ -473,15 +470,12 @@ for episode in tqdm(range(1, EPISODES + 1), ascii=True, unit='episodes'): # asci
     while not done:
 
         # This part stays mostly the same, the change is to query a model for Q values
-        # if np.random.random() > epsilon:
-        #     # Get action from Q table
-        #     action = np.argmax(agent.get_qs(current_state))
-        # else:
-        #     # Get random action
-        #     action = np.random.randint(0, env.ACTION_SPACE_SIZE)
-
-        #no exploration in testing model
-        action = np.argmax(agent.get_qs(current_state))
+        if np.random.random() > epsilon:
+            # Get action from Q table
+            action = np.argmax(agent.get_qs(current_state))
+        else:
+            # Get random action
+            action = np.random.randint(0, env.ACTION_SPACE_SIZE)
 
         new_state, reward, done = env.step(action)
 
@@ -493,10 +487,7 @@ for episode in tqdm(range(1, EPISODES + 1), ascii=True, unit='episodes'): # asci
 
         # Every step we update replay memory and train main network
         agent.update_replay_memory((current_state, action, reward, new_state, done)) # every step we have to add that to the replay memory
-        # agent.train(done, step)
-        agent.test(done, step)
-        # with open("logs.txt", 'a') as out:
-        #     out .write("step count = " + str(step) + ", episode count = " + str(episode) + '\n')
+        agent.train(done, step)
         current_state = new_state
         step += 1
     
@@ -509,20 +500,19 @@ for episode in tqdm(range(1, EPISODES + 1), ascii=True, unit='episodes'): # asci
     if not episode % AGGREGATE_STATS_EVERY or episode == 1:
     #if True:
         average_reward = sum(ep_rewards[-AGGREGATE_STATS_EVERY:])/len(ep_rewards[-AGGREGATE_STATS_EVERY:])
-        print("reward in current ep:" + str(average_reward))
-        # min_reward = min(ep_rewards[-AGGREGATE_STATS_EVERY:])
-        # max_reward = max(ep_rewards[-AGGREGATE_STATS_EVERY:])
+        min_reward = min(ep_rewards[-AGGREGATE_STATS_EVERY:])
+        max_reward = max(ep_rewards[-AGGREGATE_STATS_EVERY:])
 #        print("avg reward: " + str(average_reward))
 #        print("min reward: " + str(min_reward))
 #        print("max reward: " + str(max_reward))
-        # agent.tensorboard.update_stats(reward_avg=average_reward, reward_min=min_reward, reward_max=max_reward, epsilon=epsilon)
+        agent.tensorboard.update_stats(reward_avg=average_reward, reward_min=min_reward, reward_max=max_reward, epsilon=epsilon)
 
         # Save model, but only when min reward is greater or equal a set value
         # if min_reward >= MIN_REWARD:
-        # if episode % 50 == 0:
-        #     agent.model.save(f'models/{MODEL_NAME}__{max_reward:_>7.2f}max_{average_reward:_>7.2f}avg_{min_reward:_>7.2f}min__{int(time.time())}.model')
+        if episode % AGGREGATE_STATS_EVERY == 0:
+            agent.model.save(f'models/{MODEL_NAME}__{max_reward:_>7.2f}max_{average_reward:_>7.2f}avg_{min_reward:_>7.2f}min__{int(time.time())}.model')
 
     # Decay epsilon
-    # if epsilon > MIN_EPSILON:
-    #     epsilon *= EPSILON_DECAY
-    #     epsilon = max(MIN_EPSILON, epsilon)
+    if epsilon > MIN_EPSILON:
+        epsilon *= EPSILON_DECAY
+        epsilon = max(MIN_EPSILON, epsilon)
